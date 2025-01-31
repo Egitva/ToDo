f637ad2e39c33480d4e03f2024701e066c7b8f46
Egitva <9026479948e@gmail.com>
2025-01-31 11:21:39 +0500

    тот самый

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ class TaskManager(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("ToDo Manager")
-        self.geometry(400x400)
+        self.geometry('400x400')
         self.tasks = []
-        self.task_label = ()
-        self.task_entry = ()
-        self.task_button = ()
+        self.task_label = tk.Label(self, text="")
+        self.task_entry = tk.Entry(self)
+        self.task_button = tk.Button(self, text="", command=self.add_task)
         self.task_Listbox =tk.Listbox(self, height=10)
         self.delete_button =tk.Button(self, text="", command=self.delete_task)
+        self.edit_button =tk.Button()
+        self.mark_coplete_button =tk.Button()
+    def delete_task(self):
+        try:
+            selected_task_index =
